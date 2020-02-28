30894c77a5d0d23852bc3bb9263c358e7bd3e990
macgibbons <macgibbons@gmail.com>
2020-02-28 13:43:45 -0600

    Program prompts user for information and displays it

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace plan_your_heist
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Plan your Heist!");
+            Console.WriteLine ("Enter your teammates name");
+            Dictionary<string, string> teamMember = new Dictionary<string, string> ();
+            teamMember.Add ("Name", Console.ReadLine ());
+
+            Console.WriteLine ("Enter your teammates skill level");
+            Dictionary<string, int> memberSkill = new Dictionary<string, int> ();
+            teamMember.Add ("Skill", Console.ReadLine ());
+
+            Console.WriteLine ("Enter your teammates courage level 0.0 - 2.0 ");
+            Dictionary<string, decimal> memberCourage = new Dictionary<string, decimal> ();
+            teamMember.Add ("Courage", Console.ReadLine ());
+
+            foreach (KeyValuePair<string, string> name in teamMember)
+            {
+                Console.WriteLine ($"{name.Key}: {name.Value}");
+            }
+        }
+    }
+}
