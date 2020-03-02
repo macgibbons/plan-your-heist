c4207237ec15760b617662f407c2f3451bc53dc2
macgibbons <macgibbons@gmail.com>
2020-03-02 10:45:23 -0600

    Phase 5 is complete. User now inputs how many trials they will do and the bank difficulty changes every run

--- Program.cs
+++ Program.cs
@@ -13,57 +13,64 @@ namespace plan_your_heist
 
             // Dictionary<string, int> bankDifficulty = new Dictionary<string, int> ();
             // bankDifficulty.Add ("Difficulty", 100);
+            int teamSkill = 0;
+            Console.WriteLine ("How many Times are you going to try?");
+            int trials = int.Parse (Console.ReadLine ());
+
+            for (int i = 0; i < trials; i++)
+            {
+                
             Random rand = new Random ();
             int heistLuck = rand.Next (-10, 11);
             int bankDifficulty = 100 + heistLuck;
-            int teamSkill = 0;
 
-            while (true)
-            {
-
-                Dictionary<string, string> teamMember = new Dictionary<string, string> ();
-                Console.WriteLine ("Enter your teammates name");
-                string singleMemberName = Console.ReadLine ();
-                if (singleMemberName == "")
+                while (true)
                 {
-                    break;
-                }
 
-                teamMember.Add ("Name", singleMemberName);
-
-                Console.WriteLine ("Enter your teammates skill level");
-                teamMember.Add ("Skill", Console.ReadLine ());
-
-                Console.WriteLine ("Enter your teammates courage level 0.0 - 2.0 ");
-                teamMember.Add ("Courage", Console.ReadLine ());
-
-                teamMembers.Add (teamMember);
-
-            }
-            // iterating over the team members to add all of their skills and storing them in teamSkill
-            foreach (Dictionary<string, string> member in teamMembers)
-            {
-                foreach (KeyValuePair<string, string> individual in member)
-                {
-                    if (individual.Key == "Skill")
+                    Dictionary<string, string> teamMember = new Dictionary<string, string> ();
+                    Console.WriteLine ("Enter your teammates name");
+                    string singleMemberName = Console.ReadLine ();
+                    if (singleMemberName == "")
                     {
-                        teamSkill += int.Parse (individual.Value);
-
+                        break;
                     }
+
+                    teamMember.Add ("Name", singleMemberName);
+
+                    Console.WriteLine ("Enter your teammates skill level");
+                    teamMember.Add ("Skill", Console.ReadLine ());
+
+                    Console.WriteLine ("Enter your teammates courage level 0.0 - 2.0 ");
+                    teamMember.Add ("Courage", Console.ReadLine ());
+
+                    teamMembers.Add (teamMember);
+
                 }
+                // iterating over the team members to add all of their skills and storing them in teamSkill
+                foreach (Dictionary<string, string> member in teamMembers)
+                {
+                    foreach (KeyValuePair<string, string> individual in member)
+                    {
+                        if (individual.Key == "Skill")
+                        {
+                            teamSkill += int.Parse (individual.Value);
 
-            }
-            Console.WriteLine ($"Team Skill Level: {teamSkill}");
-            Console.WriteLine ($"Bank Difficulty Level: {bankDifficulty}");
-            // comparing the sum of the team members skills to the difficulty of the bank
-            if (teamSkill >= bankDifficulty)
-            {
-                Console.WriteLine ("You succesfully robbed the bank! 💰");
-            }
-            else
-            {
-                Console.WriteLine ("Your heist failed 💣");
+                        }
+                    }
 
+                }
+                Console.WriteLine ($"Team Skill Level: {teamSkill}");
+                Console.WriteLine ($"Bank Difficulty Level: {bankDifficulty}");
+                // comparing the sum of the team members skills to the difficulty of the bank
+                if (teamSkill >= bankDifficulty)
+                {
+                    Console.WriteLine ("You succesfully robbed the bank! 💰");
+                }
+                else
+                {
+                    Console.WriteLine ("Your heist failed 💣");
+
+                }
             }
         }
     }
