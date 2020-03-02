94acbf0d4164043820d808c169df432ac44147e6
macgibbons <macgibbons@gmail.com>
2020-03-02 09:36:21 -0600

    phase 2 complete kind of. not displaying the first persons info

--- Program.cs
+++ Program.cs
@@ -8,21 +8,47 @@ namespace plan_your_heist
         static void Main (string[] args)
         {
             Console.WriteLine ("Plan your Heist!");
-            Console.WriteLine ("Enter your teammates name");
-            Dictionary<string, string> teamMember = new Dictionary<string, string> ();
-            teamMember.Add ("Name", Console.ReadLine ());
 
-            Console.WriteLine ("Enter your teammates skill level");
-            Dictionary<string, int> memberSkill = new Dictionary<string, int> ();
-            teamMember.Add ("Skill", Console.ReadLine ());
+            Dictionary<int, string> teamMember = new Dictionary<int, string> ();
+            Dictionary<int, int> memberSkill = new Dictionary<int, int> ();
+            Dictionary<int, decimal> memberCourage = new Dictionary<int, decimal> ();
 
-            Console.WriteLine ("Enter your teammates courage level 0.0 - 2.0 ");
-            Dictionary<string, decimal> memberCourage = new Dictionary<string, decimal> ();
-            teamMember.Add ("Courage", Console.ReadLine ());
-
-            foreach (KeyValuePair<string, string> name in teamMember)
+            while (true)
             {
-                Console.WriteLine ($"{name.Key}: {name.Value}");
+
+                Console.WriteLine ("Enter your teammates name");
+                string singleMemberName = Console.ReadLine ();
+                if (singleMemberName == "")
+                {
+                    break;
+                }
+
+                teamMember.Add ((teamMember.Count + 1), singleMemberName);
+
+                Console.WriteLine ("Enter your teammates skill level");
+                memberSkill.Add ((teamMember.Count + 1), int.Parse (Console.ReadLine ()));
+
+                Console.WriteLine ("Enter your teammates courage level 0.0 - 2.0 ");
+                memberCourage.Add ((teamMember.Count + 1), decimal.Parse (Console.ReadLine ()));
+
+            }
+            foreach (KeyValuePair<int, string> name in teamMember)
+            {
+                // Console.WriteLine ($"{name.Key}: {name.Value}");
+                foreach (KeyValuePair<int, int> skill in memberSkill)
+                {
+                    if (skill.Key == name.Key)
+                    {
+                        foreach (KeyValuePair<int, decimal> courage in memberCourage)
+                        {
+                            if (courage.Key == name.Key)
+                            {
+
+                                Console.WriteLine ($"{name.Value} skilllevel: {skill.Value}, courageLevel: {courage.Value}");
+                            }
+                        }
+                    }
+                }
             }
         }
     }
